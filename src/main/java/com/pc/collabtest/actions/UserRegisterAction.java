679ce55aecc1094763d8486cd8d0e9d641508c37
pratikwaghela1 <pratikwaghela1@gmail.com>
2022-05-13 14:58:00 +0530

    field validation and keep same url if error occured

--- src/main/java/com/pc/collabtest/actions/UserRegisterAction.java
+++ src/main/java/com/pc/collabtest/actions/UserRegisterAction.java
@@ -15,35 +15,27 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class UserRegisterAction extends ActionSupport {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	@Valid
 	public User user;
 
-	
+	@Autowired
+	private UserService userService;
 
-	private String userName;
-	
-	
 	public String userRegisterForm() throws Exception {
 		log.info("userRegisterForm() START");
 		return "success";
 	}
 
 	public String userRegister() throws Exception {
-		log.info("userName " + userName);
-		log.info("userRegister() START ********************");
 		log.info("userRegister() START ********************");
 		log.info("user - {}", user);
-
 		if (user != null) {
-			return "userList";
-//			if(userService.saveUser(user) != null) {
-//				
-//			}
+
+			if (userService.saveUser(user) != null) {
+				return "userList";
+			}
 		}
 		return "userRegisterForm";
 	}
@@ -56,16 +48,6 @@ public class UserRegisterAction extends ActionSupport {
 	public void setUser(User user) {
 		log.info("setUserCall ................");
 		this.user = user;
-	}
-
-	public String getUserName() {
-		log.info("getUserName ................");
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		log.info("setUserName ................");
-		this.userName = userName;
 	}
 
 }
